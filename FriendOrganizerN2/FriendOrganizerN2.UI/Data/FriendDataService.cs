3204e1f7b5dbb3613b8e2e1d1de32103752aaf4e
levon0101 <levon.ecs@gmail.com>
2018-04-09 02:01:57 +0400

    4_4

---
+++ FriendOrganizerN2/FriendOrganizerN2.UI/Data/FriendDataService.cs
@@ -0,0 +1,23 @@
+﻿using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganizerN2.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+
+        //using Named Interator for organize data service
+        public IEnumerable<Friend> GetAll()
+        {
+            // To Do
+            yield return new Friend { FirstName = "Rafael", LastName = "Grigoryan", Email = "--" };
+            yield return new Friend { FirstName = "Karen", LastName = "Barseghyan", Email = "--" };
+            yield return new Friend { FirstName = "Stive", LastName = "Voznyak", Email = "--" };
+            yield return new Friend { FirstName = "Gelelo", LastName = "Galelei", Email = "--" };
+        }
+    }
+}
---
+++ FriendOrganizerN2/FriendOrganizerN2.UI/Data/IFriendDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizerN2.UI.Data
+{
+    public interface IFriendDataService
+    {
+        IEnumerable<Friend> GetAll();
+    }
+}
